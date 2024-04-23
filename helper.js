dea7f2a2d165e4287c503a48eb609264c5fac8f8
Gaurav Sharma <gaurav.sharma@pocketfm.com>
2024-04-23 21:04:48 +0530

    Updated helper.js

--- helper.js
+++ helper.js
@@ -11,3 +11,7 @@
 // to stage changes --> git add ( __FILE__NAME__ or . (all files) )
 // git commit -m "__COMMIT_MESSAGE__" --> staged changes are committed
 // if we want to check the log ---> git log
+// to check the status of the current directory --> git status
+// git remote add origin ( main git repo ) __URL_OF_THE_REPO__
+// git push -u origin main
+// git push ( pushing our changes ) -u ( use it whenever we are pushing a new branch ) origin main (branch name)
