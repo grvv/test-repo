e2f266b6b0aedb9199741108e5dcd7c98f5155cf
Gaurav Sharma <gaurav.sharma@pocketfm.com>
2024-04-23 20:53:44 +0530

    Added Helper file

---
+++ helper.js
@@ -0,0 +1,11 @@
+// Commands
+
+// mkdir --> make directory
+// touch __FILE__NAME__ --> to create a file
+// ls --> list down content of the directory
+
+// Git
+
+// Create a new repo on github.com
+// git init --> initializes git
+// to stage changes --> git add ( __FILE__NAME__ or . (all files) )
